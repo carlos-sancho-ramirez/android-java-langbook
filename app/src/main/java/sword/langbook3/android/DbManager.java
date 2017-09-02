4827cf1ac37d0440e6f39e56f1a757597582cbf4
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2017-09-02 12:37:07 +0200

    Read languages from database

--- app/src/main/java/sword/langbook3/android/DbManager.java
+++ app/src/main/java/sword/langbook3/android/DbManager.java
@@ -1,12 +1,16 @@
 package sword.langbook3.android;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.SparseIntArray;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 
 import sword.bitstream.FunctionWithIOException;
 import sword.bitstream.HuffmanTable;
@@ -87,34 +91,153 @@ public class DbManager extends SQLiteOpenHelper {
         }
     }
 
+    private static final class TableNames {
+        static final String symbolArrays = "SymbolArrays";
+    }
+
+    private String getSymbolArray(SQLiteDatabase db, int id) {
+        final String whereClause = "id = ?";
+        Cursor cursor = db.query(TableNames.symbolArrays, new String[] {"str"}, whereClause,
+                new String[] { Integer.toString(id) }, null, null, null, null);
+        if (cursor != null) {
+            try {
+                final int count = cursor.getCount();
+                if (count > 1) {
+                    throw new AssertionError("There should not be repeated identifiers");
+                }
+
+                if (count > 0 && cursor.moveToFirst()) {
+                    return cursor.getString(0);
+                }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+
+        return null;
+    }
+
+    private Integer getSymbolArray(SQLiteDatabase db, String str) {
+        final String whereClause = "str = ?";
+        Cursor cursor = db.query(TableNames.symbolArrays, new String[] {"id"}, whereClause,
+                new String[] { str }, null, null, null, null);
+        if (cursor != null) {
+            try {
+                final int count = cursor.getCount();
+                if (count > 1) {
+                    throw new AssertionError("There should not be repeated symbol arrays");
+                }
+
+                if (count > 0 && cursor.moveToFirst()) {
+                    return cursor.getInt(0);
+                }
+            }
+            finally {
+                cursor.close();
+            }
+        }
+
+        return null;
+    }
+
+    private int insertSymbolArray(SQLiteDatabase db, String str) {
+        db.execSQL("INSERT INTO " + TableNames.symbolArrays + " (str) VALUES ('" + str + "')");
+        final Integer id = getSymbolArray(db, str);
+        if (id == null) {
+            throw new AssertionError("A just introduced register should be found");
+        }
+
+        return id;
+    }
+
+    private int insertIfNotExists(SQLiteDatabase db, String str) {
+        final Integer id = getSymbolArray(db, str);
+        if (id != null) {
+            return id;
+        }
+
+        return insertSymbolArray(db, str);
+    }
+
+    private int[] readSymbolArrays(SQLiteDatabase db, InputBitStream ibs) throws IOException {
+        final int symbolArraysLength = (int) ibs.readNaturalNumber();
+        final HuffmanTable<Long> nat3Table = new NaturalNumberHuffmanTable(3);
+        final HuffmanTable<Long> nat4Table = new NaturalNumberHuffmanTable(4);
+
+        final HuffmanTable<Character> charHuffmanTable =
+                ibs.readHuffmanTable(new CharReader(ibs), new CharDiffReader(ibs, nat4Table));
+
+        final HuffmanTable<Integer> symbolArraysLengthTable =
+                ibs.readHuffmanTable(new IntReader(ibs), new IntDiffReader(ibs, nat3Table));
+
+        final int[] idMap = new int[symbolArraysLength];
+        for (int index = 0; index < symbolArraysLength; index++) {
+            final int length = ibs.readHuffmanSymbol(symbolArraysLengthTable);
+            final StringBuilder builder = new StringBuilder();
+            for (int pos = 0; pos < length; pos++) {
+                builder.append(ibs.readHuffmanSymbol(charHuffmanTable));
+            }
+
+            idMap[index] = insertIfNotExists(db, builder.toString());
+        }
+
+        return idMap;
+    }
+
+    private static final class StreamedDatabaseConstants {
+        static final int minValidAlphabet = 3;
+    }
+
+    static final class Language {
+
+        private final String _code;
+        private final int _minAlphabet;
+        private final int _maxAlphabet;
+
+        Language(String code, int minAlphabet, int alphabetCount) {
+            if (code.length() != 2) {
+                throw new IllegalArgumentException("Invalid language code");
+            }
+
+            if (alphabetCount <= 0) {
+                throw new IllegalArgumentException("Alphabet count must be positive");
+            }
+
+            _code = code;
+            _minAlphabet = minAlphabet;
+            _maxAlphabet = minAlphabet + alphabetCount - 1;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + _code + ", " + Integer.toString(_maxAlphabet - _minAlphabet + 1) + ')';
+        }
+    }
+
+    public Language[] languages;
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE SymbolArrays (id INTEGER PRIMARY KEY AUTOINCREMENT, str TEXT)");
+        db.execSQL("CREATE TABLE " + TableNames.symbolArrays + " (id INTEGER PRIMARY KEY AUTOINCREMENT, str TEXT)");
 
         final InputStream is = _context.getResources().openRawResource(R.raw.basic);
         try {
             is.skip(20);
             final InputBitStream ibs = new InputBitStream(is);
+            final int[] symbolArraysIdMap = readSymbolArrays(db, ibs);
+            final int maxSymbolArrayIndex = symbolArraysIdMap.length - 1;
 
-            final int symbolArraysLength = (int) ibs.readNaturalNumber();
-            final HuffmanTable<Long> nat3Table = new NaturalNumberHuffmanTable(3);
-            final HuffmanTable<Long> nat4Table = new NaturalNumberHuffmanTable(4);
-
-            final HuffmanTable<Character> charHuffmanTable =
-                    ibs.readHuffmanTable(new CharReader(ibs), new CharDiffReader(ibs, nat4Table));
-
-            final HuffmanTable<Integer> symbolArraysLengthTable =
-                    ibs.readHuffmanTable(new IntReader(ibs), new IntDiffReader(ibs, nat3Table));
-
-            for (int index = 0; index < symbolArraysLength; index++) {
-                final int length = ibs.readHuffmanSymbol(symbolArraysLengthTable);
-                final StringBuilder builder = new StringBuilder();
-                for (int pos = 0; pos < length; pos++) {
-                    builder.append(ibs.readHuffmanSymbol(charHuffmanTable));
-                }
-
-                final String str = builder.toString();
-                db.execSQL("INSERT INTO SymbolArrays (str) VALUES ('" + str + "')");
+            final int languageCount = (int) ibs.readNaturalNumber();
+            languages = new Language[languageCount];
+            int minAlphabet = StreamedDatabaseConstants.minValidAlphabet;
+            final HuffmanTable<Long> nat2Table = new NaturalNumberHuffmanTable(2);
+            for (int languageIndex = 0; languageIndex < languageCount; languageIndex++) {
+                final int codeSymbolArrayIndex = ibs.readRangedNumber(0, maxSymbolArrayIndex);
+                final int alphabetCount = ibs.readHuffmanSymbol(nat2Table).intValue();
+                final String code = getSymbolArray(db, symbolArraysIdMap[codeSymbolArrayIndex]);
+                languages[languageIndex] = new Language(code, minAlphabet, alphabetCount);
+                minAlphabet += alphabetCount;
             }
         }
         catch (IOException e) {
--- app/src/main/java/sword/langbook3/android/SearchActivity.java
+++ app/src/main/java/sword/langbook3/android/SearchActivity.java
@@ -13,19 +13,24 @@ public class SearchActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
 
-        SQLiteDatabase db = new DbManager(this).getReadableDatabase();
+        final StringBuilder builder = new StringBuilder();
+        final DbManager dbManager = new DbManager(this);
+        SQLiteDatabase db = dbManager.getReadableDatabase();
+
+        DbManager.Language[] languages = dbManager.languages;
+        for (DbManager.Language language : languages) {
+            builder.append(language.toString()).append('\n');
+        }
+
         try {
             final String whereClause = null;
             Cursor cursor = db.query("SymbolArrays", new String[] {"id", "str"}, whereClause, null, null, null, null);
             if (cursor != null) {
                 try {
                     if (cursor.getCount() > 0 && cursor.moveToFirst()) {
-                        StringBuilder builder = new StringBuilder();
                         do {
                             builder.append(cursor.getInt(0)).append(": ").append(cursor.getString(1)).append('\n');
                         } while (cursor.moveToNext());
-                        final TextView textView = (TextView) findViewById(R.id.textView);
-                        textView.setText(builder.toString());
                     }
                 }
                 finally {
@@ -36,5 +41,8 @@ public class SearchActivity extends Activity {
         finally {
             db.close();
         }
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(builder.toString());
     }
 }
