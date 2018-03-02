7dde081645a6faef9439b02143bfc13492dfbe85
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2018-03-02 18:28:15 +0100

    SQLite database can be exported

--- app/src/main/java/sword/langbook3/android/SettingsActivity.java
+++ app/src/main/java/sword/langbook3/android/SettingsActivity.java
@@ -1,21 +1,27 @@
 package sword.langbook3.android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class SettingsActivity extends Activity implements View.OnClickListener, DbManager.DatabaseImportProgressListener {
+import java.io.IOException;
+
+public class SettingsActivity extends Activity implements View.OnClickListener, DbManager.DatabaseImportProgressListener, DialogInterface.OnClickListener {
 
     private static final int REQUEST_CODE_PICK_FILE = 1;
 
     private boolean _resumed;
+    private EditText _dialogEditText;
 
     public static void open(Context context) {
         Intent intent = new Intent(context, SettingsActivity.class);
@@ -31,6 +37,18 @@ public class SettingsActivity extends Activity implements View.OnClickListener, 
         else {
             Toast.makeText(this, "No file picker application found in the device", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void selectLocation() {
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.selectLocationDialogTitle)
+                .setPositiveButton(R.string.selectLocationDialogOk, this)
+                .create();
+        final View dialogView = LayoutInflater.from(dialog.getContext()).inflate(R.layout.select_location_dialog, null);
+        _dialogEditText = dialogView.findViewById(R.id.filePath);
+
+        dialog.setView(dialogView);
+        dialog.show();
     }
 
     private void importDatabase(Uri uri) {
@@ -51,8 +69,10 @@ public class SettingsActivity extends Activity implements View.OnClickListener, 
         final View progressPanel = findViewById(R.id.progressPanel);
         progressPanel.setOnClickListener(this);
 
-        final Button importDatabaseButton = findViewById(R.id.importDatabaseButton);
-        importDatabaseButton.setOnClickListener(this);
+        findViewById(R.id.importStreamedDatabaseButton).setOnClickListener(this);
+        findViewById(R.id.exportStreamedDatabaseButton).setOnClickListener(this);
+        findViewById(R.id.importSqliteDatabaseButton).setOnClickListener(this);
+        findViewById(R.id.exportSqliteDatabaseButton).setOnClickListener(this);
     }
 
     @Override
@@ -79,8 +99,12 @@ public class SettingsActivity extends Activity implements View.OnClickListener, 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.importDatabaseButton:
+            case R.id.importStreamedDatabaseButton:
                 pickFile();
+                break;
+
+            case R.id.exportSqliteDatabaseButton:
+                selectLocation();
                 break;
 
             case R.id.progressPanel:
@@ -109,11 +133,27 @@ public class SettingsActivity extends Activity implements View.OnClickListener, 
         progressPanel.setVisibility(inProgress? View.VISIBLE : View.GONE);
     }
 
+    private void saveSqliteDatabase(String filePath) {
+        try {
+            Utils.copyFile(getDatabasePath(DbManager.DB_NAME).toString(), filePath);
+            Toast.makeText(this, R.string.exportSuccess, Toast.LENGTH_SHORT).show();
+        }
+        catch (IOException e) {
+            Toast.makeText(this, R.string.exportFailure, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     public void onPause() {
         DbManager.getInstance().setProgressListener(null);
         _resumed = false;
 
         super.onPause();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        saveSqliteDatabase(_dialogEditText.getText().toString());
+        dialog.dismiss();
     }
 }
