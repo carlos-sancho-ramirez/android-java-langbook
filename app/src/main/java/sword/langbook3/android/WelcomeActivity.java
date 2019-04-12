7d4f5e3e97e39294021528bf19be48ff8ca58b77
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2019-04-12 17:16:30 +0200

    Include button to add first language in the welcome screen

--- app/src/main/java/sword/langbook3/android/WelcomeActivity.java
+++ app/src/main/java/sword/langbook3/android/WelcomeActivity.java
@@ -3,8 +3,11 @@ package sword.langbook3.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public final class WelcomeActivity extends Activity {
+public final class WelcomeActivity extends Activity implements View.OnClickListener {
+
+    private static final int REQUEST_CODE_ADD_LANGUAGE = 1;
 
     public static void open(Activity activity, int requestCode) {
         final Intent intent = new Intent(activity, WelcomeActivity.class);
@@ -15,5 +18,19 @@ public final class WelcomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
+
+        findViewById(R.id.addLanguageButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        LanguageAdderActivity.open(this, REQUEST_CODE_ADD_LANGUAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_ADD_LANGUAGE && resultCode == RESULT_OK) {
+            finish();
+        }
     }
 }
