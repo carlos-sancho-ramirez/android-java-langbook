7fe26ff2d52b1dbf44df339a90bc6a912b158e56
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2020-01-02 16:06:29 +0100

    Include test for serialization of a single agent copying a single acceptation from source to target bunch

--- app/src/test/java/sword/langbook3/android/sdb/AgentsSerializerTest.java
+++ app/src/test/java/sword/langbook3/android/sdb/AgentsSerializerTest.java
@@ -69,4 +69,52 @@ public abstract class AgentsSerializerTest extends BunchesSerializerTest {
         assertEmpty(outManager.getAcceptationsInBunch(outArVerbConcept));
         assertEmpty(outManager.getAcceptationsInBunch(outVerbConcept));
     }
+
+    @Test
+    public void testSerializeCopyFromSingleSourceToTargetAgentWithAMatchingAcceptation() {
+        final MemoryDatabase inDb = new MemoryDatabase();
+        final AgentsManager inManager = createManager(inDb);
+
+        final int inAlphabet = inManager.addLanguage("es").mainAlphabet;
+        final int arVerbBunch = inManager.getMaxConcept() + 1;
+        addSimpleAcceptation(inManager, inAlphabet, arVerbBunch, "verbo de primera conjugación");
+
+        final int verbBunch = inManager.getMaxConcept() + 1;
+        addSimpleAcceptation(inManager, inAlphabet, verbBunch, "verbo");
+
+        final int inSingConcept = inManager.getMaxConcept() + 1;
+        final int inAcceptation = addSimpleAcceptation(inManager, inAlphabet, inSingConcept, "cantar");
+        assertTrue(inManager.addAcceptationInBunch(arVerbBunch, inAcceptation));
+
+        final ImmutableIntSet noBunches = ImmutableIntArraySet.empty();
+        final ImmutableIntSet sourceBunches = noBunches.add(arVerbBunch);
+        final ImmutableIntKeyMap<String> emptyCorrelation = ImmutableIntKeyMap.empty();
+        assertNotNull(inManager.addAgent(verbBunch, sourceBunches, noBunches, emptyCorrelation, emptyCorrelation, emptyCorrelation, emptyCorrelation, 0));
+
+        final MemoryDatabase outDb = cloneBySerializing(inDb);
+        final AgentsManager outManager = new LangbookDatabaseManager(outDb);
+
+        final int outAgentId = getSingleInt(outManager.getAgentIds());
+
+        final int outArVerbAcceptation = getSingleInt(findAcceptationsMatchingText(outDb, "verbo de primera conjugación"));
+        final int outArVerbConcept = outManager.conceptFromAcceptation(outArVerbAcceptation);
+
+        final int outVerbAcceptation = getSingleInt(findAcceptationsMatchingText(outDb, "verbo"));
+        final int outVerbConcept = outManager.conceptFromAcceptation(outVerbAcceptation);
+        assertNotEquals(outArVerbConcept, outVerbConcept);
+
+        final AgentDetails outAgentDetails = outManager.getAgentDetails(outAgentId);
+        assertEquals(outVerbConcept, outAgentDetails.targetBunch);
+        assertEquals(1, outAgentDetails.sourceBunches.size());
+        assertEquals(outArVerbConcept, outAgentDetails.sourceBunches.valueAt(0));
+        assertEmpty(outAgentDetails.diffBunches);
+        assertEmpty(outAgentDetails.startMatcher);
+        assertEmpty(outAgentDetails.startAdder);
+        assertEmpty(outAgentDetails.endMatcher);
+        assertEmpty(outAgentDetails.endAdder);
+
+        final int outSingAcceptation = getSingleInt(findAcceptationsMatchingText(outDb, "cantar"));
+        assertSingleInt(outSingAcceptation, outManager.getAcceptationsInBunch(outArVerbConcept));
+        assertSingleInt(outSingAcceptation, outManager.getAcceptationsInBunch(outVerbConcept));
+    }
 }
