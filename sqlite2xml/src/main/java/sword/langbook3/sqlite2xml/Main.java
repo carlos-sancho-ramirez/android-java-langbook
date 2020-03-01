13fabfbe2d8d33332198fd4beac25698fbd605ae
Carlos Sancho Ramírez <carlos.sancho.ramirez@gmail.com>
2020-03-01 17:28:59 +0100

    Include sqlite2xml module with a Hello world application

---
+++ sqlite2xml/src/main/java/sword/langbook3/sqlite2xml/Main.java
@@ -0,0 +1,10 @@
+package sword.langbook3.sqlite2xml;
+
+public final class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    private Main() {
+    }
+}
