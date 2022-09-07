52041f7b2bb435baccd3b1d8fde711163b8a1c99
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2022-09-07 16:48:12 +0200

    Allow again quizzes for the null bunch

--- dbManager/src/main/java/sword/langbook3/android/models/QuizDetails.java
+++ dbManager/src/main/java/sword/langbook3/android/models/QuizDetails.java
@@ -2,14 +2,15 @@ package sword.langbook3.android.models;
 
 import sword.collections.ImmutableList;
 
+import static sword.collections.SortUtils.equal;
+import static sword.langbook3.android.util.PreconditionUtils.ensureValidArguments;
+
 public final class QuizDetails<AlphabetId, BunchId, RuleId> {
     public final BunchId bunch;
     public final ImmutableList<QuestionFieldDetails<AlphabetId, RuleId>> fields;
 
     public QuizDetails(BunchId bunch, ImmutableList<QuestionFieldDetails<AlphabetId, RuleId>> fields) {
-        if (bunch == null || fields == null || fields.size() < 2 || !fields.anyMatch(field -> !field.isAnswer()) || !fields.anyMatch(QuestionFieldDetails::isAnswer)) {
-            throw new IllegalArgumentException();
-        }
+        ensureValidArguments(fields != null && fields.size() >= 2 && !fields.allMatch(QuestionFieldDetails::isAnswer) && fields.anyMatch(QuestionFieldDetails::isAnswer));
 
         this.bunch = bunch;
         this.fields = fields;
@@ -17,7 +18,7 @@ public final class QuizDetails<AlphabetId, BunchId, RuleId> {
 
     @Override
     public int hashCode() {
-        return fields.hashCode() * 41 + bunch.hashCode();
+        return fields.hashCode() * 41 + ((bunch != null)? bunch.hashCode() : 0);
     }
 
     @Override
@@ -27,7 +28,7 @@ public final class QuizDetails<AlphabetId, BunchId, RuleId> {
         }
 
         final QuizDetails that = (QuizDetails) other;
-        return bunch.equals(that.bunch) && fields.equals(that.fields);
+        return equal(bunch, that.bunch) && fields.equals(that.fields);
     }
 
     @Override
